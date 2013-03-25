00ead3f92b246f67d8a10864239475778145a56c
John Molyneux <johncmolyneux@hotmail.com>
2013-03-25 17:09:47 +0000

    Modified database insert

--- www/js/index.js
+++ www/js/index.js
@@ -91,34 +91,26 @@ function readAsText(file) {
 		}
 
 		function successCB() {
-			var sql = "";
+			var startTime = new Date();
+			
+            html5sql.openDatabase("Easitill", "Easitill DB", _dbSize);
 			for (var i = 0; i < lines.length; i++) {
-				//var pd = i / lines.length * 100;
-				//$(".progress-bar").css("width", pd + "%");
+				var pd = i / lines.length * 100;
+				$(".progress-bar").css("width", pd + "%");
 				var line = lines[i].split(",");
-				sql += "INSERT INTO veprods (linecode, description, barcode, price, stock) values (" +
+				var sql = "INSERT INTO veprods (linecode, description, barcode, price, stock) values (" +
 					line[0] + ", " +
 					"'" + line[2] + "', " +
 					"'" + line[1] + "', " +
 					line[6] + "," + 
-					line[13] + ");";
-			}
-            try {
-                html5sql.openDatabase("Easitill", "Easitill DB", _dbSize);
-				var startTime = new Date();
-                html5sql.process(
-					sql,
-					function(){ //Success
-						var endTime = new Date();
-						alert("Database created in " + ((endTime - startTime) / 1000) + " seconds");
-					},
-					function(error, failingQuery){ //Failure
-						alert("Error : " + error.message);
-					}
+					line[13] + ")";
+                html5sql.process(sql);
 				);
-            } catch (error) {
-				alert("Exception : " + error.message);
-            }
+			}
+			
+			var endTime = new Date();
+			
+			alert("Database populated in " + ((endTime - startTime) / 1000) + " seconds");
 		}
 	};
 	reader.readAsText(file);
